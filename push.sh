e7c792148957351f30c2e970963557dd19bb2faf
Adrian Ding <a.ding@gns.cri.nz>
2020-12-11 11:32:27 +1300

    Update to static Dockerfile

--- push.sh
+++ push.sh
@@ -2,12 +2,11 @@
 
 # Builds and pushes Docker images for the arg list.
 #
-# usage: ./build-push.sh project [project]
-
-./build.sh $@
+# usage: ./push.sh project [project]
 
 ACCOUNT=$(aws sts get-caller-identity --output text --query 'Account')
 VERSION='git-'`git rev-parse --short HEAD`
+
 eval $(aws ecr get-login --no-include-email --region ap-southeast-2)
 
 for i in "$@"
