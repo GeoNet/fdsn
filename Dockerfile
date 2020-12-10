147dae75f6e86e93a614455b3faa767b20040a2d
Adrian Ding <a.ding@gns.cri.nz>
2020-12-11 11:32:27 +1300

    Update to static Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+ARG BUILDER_IMAGE=quay.io/geonet/golang:1.13-alpine
+ARG RUNNER_IMAGE=quay.io/geonet/alpine:3.10
+ARG RUN_USER=nobody
+# Only support image based on AlpineLinux
+FROM ${BUILDER_IMAGE} as builder
+
+# Obtain ca-cert and tzdata, which we will add to the container
+RUN apk add --update ca-certificates tzdata gcc make musl-dev 
+
+# Project to build
+ARG BUILD
+
+# Git commit SHA
+ARG GIT_COMMIT_SHA
+
+COPY ./ /repo
+
+WORKDIR /repo
+
+# Set a bunch of go env flags
+ENV GOBIN /repo/gobin
+ENV GOPATH /usr/src/go
+ENV GOFLAGS -mod=vendor
+ENV GOOS linux
+ENV GOARCH amd64
+ENV CGO_ENABLED ${CGO_ENABLED}
+
+RUN echo 'nobody:x:65534:65534:Nobody:/:\' > /passwd
+RUN make -B -C vendor/github.com/GeoNet/kit/cvendor/libmseed/ && \
+  make -B -C vendor/github.com/GeoNet/kit/cvendor/liblink && \
+  go install -a -installsuffix cgo -ldflags "-X main.Prefix=${BUILD}/${GIT_COMMIT_SHA}" /repo/cmd/${BUILD}
+
+FROM ${RUNNER_IMAGE}
+# Export a port, default to 8080
+ARG EXPOSE_PORT=8080
+EXPOSE $EXPOSE_PORT
+
+# Add common resource for ssl and timezones from the build container
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=builder /usr/share/zoneinfo /usr/share/zoneinfo
+# Create a nobody user
+COPY --from=builder /passwd /etc/passwd
+# Same ARG as before
+ARG BUILD
+# Need to make this an env for it to be interpolated by the shell
+ENV TZ Pacific/Auckland
+ENV BUILD_BIN=${BUILD}
+# We have to make our binary have a fixed name, otherwise, we cannot run it without a shell
+COPY --from=builder /repo/gobin/${BUILD} /app
+# Copy the assets
+ARG ASSET_DIR
+COPY ${ASSET_DIR} /assets
+ARG RUN_USER=nobody
+USER ${RUN_USER}
+CMD ["/app"]
--- build.sh
+++ build.sh
@@ -10,82 +10,36 @@
 # relative to the executable. 
 #
 # usage: ./build.sh project [project]
-
 if [ $# -eq 0 ]; then
-    echo Error: please supply a project to build. Usage: ./build.sh project [project]
-    exit 1
+  echo Error: please supply a project to build. Usage: ./build.sh project [project]
+  exit 1
 fi
 
 # code will be compiled in this container
-BUILD_CONTAINER=quay.io/geonet/golang:1.13.1-alpine
+BUILDER_IMAGE='quay.io/geonet/golang:1.13.1-alpine'
+RUNNER_IMAGE='quay.io/geonet/alpine:3.10'
 
-# dependent resources will be gathered from this container
-RESOURCE_CONTAINER=quay.io/geonet/alpine:3.10
 
-DOCKER_TMP=docker-build-tmp
-
-mkdir -p $DOCKER_TMP
-chmod +s $DOCKER_TMP
-
-sudo rm -rf $DOCKER_TMP/*
-
+VERSION='git-'$(git rev-parse --short HEAD)
 ACCOUNT=$(aws sts get-caller-identity --output text --query 'Account')
-VERSION='git-'`git rev-parse --short HEAD`
 
-# The current working dir to use in GOBIN etc e.g., geonet-web
-CWD=${PWD##*/}
+for i in "$@"; do
+  if [ ${i} = "fdsn-ws" ] || [ ${i} = "fdsn-holdings-consumer" ] || [ ${i} = "fdsn-slink-db" ]; then
+    CGO_ENABLED = 1
+  fi
+  docker build \
+    --build-arg=BUILD="$i" \
+    --build-arg=RUNNER_IMAGE="$RUNNER_IMAGE" \
+    --build-arg=BUILDER_IMAGE="$BUILDER_IMAGE" \
+    --build-arg=GIT_COMMIT_SHA="$VERSION" \
+    --build-arg=ASSET_DIR="./cmd/$i/assets" \
+    --build-arg=CGO_ENABLED="${CGO_ENABLED}" \
+    -t "${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/${i}:$VERSION" \
+    -f "Dockerfile" .
 
-mkdir -p ${DOCKER_TMP}/etc/ssl/certs
-mkdir -p ${DOCKER_TMP}/usr/share
+  # tag latest.  Makes it easier to test with compose.
+  docker tag \
+    "${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/${i}:$VERSION" \
+    "${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/${i}:latest"
 
-# Assemble common resource for ssl and timezones from the build container
-docker run --rm -v ${PWD}:${PWD} ${RESOURCE_CONTAINER} \
-    /bin/ash -c "apk add --update ca-certificates tzdata && \
-    cp /etc/ssl/certs/ca-certificates.crt ${PWD}/${DOCKER_TMP}/etc/ssl/certs && \
-    cp -Ra /usr/share/zoneinfo ${PWD}/${DOCKER_TMP}/usr/share"
-
-# Assemble common resource for user.
-echo "nobody:x:65534:65534:Nobody:/:" > ${DOCKER_TMP}/etc/passwd
-
-for i in "$@"
-do
-
-    # only enable Cgo for executables that require it
-    enable_cgo=0
-    if [ ${i} = "fdsn-ws" ] || [ ${i} = "fdsn-holdings-consumer" ] || [ ${i} = "fdsn-slink-db" ]; then
-        enable_cgo=1
-    fi
-
-    # install dependencies to compile libmseed and libslink, and compile/install with the -static flag to statically link with C libs (if applicable)
-	docker run -e "GOBIN=/usr/src/go/src/github.com/GeoNet/${CWD}/${DOCKER_TMP}" -e "CGO_ENABLED=${enable_cgo}" -e "GOPATH=/usr/src/go" -e "GOFLAGS=-mod=vendor" -e "GOOS=linux" -e "BUILD=$BUILD" --rm \
-		-v "$PWD":/usr/src/go/src/github.com/GeoNet/${CWD} \
-		-w /usr/src/go/src/github.com/GeoNet/${CWD} ${BUILD_CONTAINER} \
-		/bin/ash -c "apk add --update ca-certificates tzdata gcc make musl-dev && \
-				     make -B -C /usr/src/go/src/github.com/GeoNet/${CWD}/vendor/github.com/GeoNet/kit/cvendor/libmseed && \
-				     make -B -C /usr/src/go/src/github.com/GeoNet/${CWD}/vendor/github.com/GeoNet/kit/cvendor/libslink && \
-		             go install -a -ldflags \"-X main.Prefix=${i}/${VERSION} -extldflags -static\" -installsuffix cgo ./cmd/${i}"
-
-		rm -rf $DOCKER_TMP/assets
-		mkdir $DOCKER_TMP/assets
-		rsync --archive --quiet --ignore-missing-args  cmd/${i}/assets docker-build-tmp/
-
-        # Add a default Dockerfile
-
-		rm -f $DOCKER_TMP/Dockerfile
-
-		echo "FROM scratch" > docker-build-tmp/Dockerfile
-		echo "ADD ./ /" >> docker-build-tmp/Dockerfile
-		echo "USER nobody" >> docker-build-tmp/Dockerfile
-		echo "EXPOSE 8080" >> docker-build-tmp/Dockerfile
-		echo "CMD [\"/${i}\"]" >> docker-build-tmp/Dockerfile
-
-        # If a project specifies a Dockerfile then copy it over the top of the default file.
-
-        rsync --ignore-missing-args cmd/${i}/Dockerfile docker-build-tmp/
-
-		docker build -t ${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/${i}:$VERSION -f docker-build-tmp/Dockerfile docker-build-tmp
-		# tag latest.  Makes it easier to test with compose. 
-		docker tag ${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/${i}:$VERSION ${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/${i}:latest
-
-		rm -f $DOCKER_TMP/$i
 done
